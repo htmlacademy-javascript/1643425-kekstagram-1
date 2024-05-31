e12341edd758f0fa548b725be1c49ebe4029f0a1
Pavel8672 <007woron@gmail.com>
2024-05-31 21:30:39 +0500

    Добавляет исправления по функциям

--- js/functions.js
+++ js/functions.js
@@ -1,81 +1,79 @@
 //функция паллиндром
-function palindromes(string) {
+const checkIsPalindrom = (string) => {
   const str = string.toLowerCase();
   const mid = Math.round(str.length / 2);
 
   for (let i = 0; i < mid; i++) {
     if (str[i] !== str[str.length - 1 - i]) {
-      return 'Не палиндром';
+      return false;
     }
   }
-  return 'Палиндром';
-}
-// Строка является палиндромом
-palindromes('топот'); // true
-// Несмотря на разный регистр, тоже палиндром
-palindromes('ДовОд'); // true
-// Это не палиндром
-palindromes('Кекс'); // false
+
+  return true;
+};
+
 
 //Функция проверки на числа
-function checkingForNumber(string) {
-  let finalLine = [];
+const parseNumber = (string) => {
+  let finalLine = '';
   const str = string.toLowerCase();
+
   for (let i = 0; i < str.length; i++) {
-
-    if (isNaN(str[i]) === false) {
-      finalLine += str[i].trim();
+    if (!isNaN(str[i])) {
+      finalLine += str[i].trim();//Если здесь убрать trim то строка '1 кефир, 0.5 батона' на выходе получается '1 05' и  строка 'ECMAScript 2022' выходит с пробелом впереди
     }
-
   }
+
   if (finalLine.length === 0) {
     return NaN;
   }
 
   return finalLine;
-}
-checkingForNumber('2023 год'); // 2023
-checkingForNumber('ECMAScript 2022'); // 2022
-checkingForNumber('1 кефир, 0.5 батона'); // 105
-checkingForNumber('агент 007'); // 7
-checkingForNumber('а я томат'); // NaN
+};
+/* console.log(parseNumber('2023 год')); // 2023
+console.log(parseNumber('ECMAScript 2022')); // 2022
+console.log(parseNumber('1 кефир, 0.5 батона')); // 105
+console.log(parseNumber('а я томат')); // NaN */
 
 
 //Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины
-function augmentedString(originalLine, minimumLength, additionalCharacters) {
-  let total = [];
-  if (originalLine.length >= minimumLength) {
+const returnsModifiedString = (originalLine, targetLength, additionalStr) => {
+  let total = '';
+
+  if (originalLine.length >= targetLength) {
     return originalLine;
-  } else {
-    total += originalLine;
-    if ((additionalCharacters + originalLine).length > minimumLength) {
-      total = additionalCharacters.slice(0, minimumLength - originalLine.length).concat(total);
-    }
-    while ((additionalCharacters + total).length <= minimumLength) {
-
-      total = additionalCharacters.concat(total);
-    }
   }
-  return total;
-}
 
-augmentedString('1', 2, '0'); // '01'
-// Добавочный символ использован три раза
-augmentedString('1', 4, '0'); // '0001'
-// Добавочные символы обрезаны с конца
-augmentedString('q', 4, 'werty'); // 'werq'
-// Добавочные символы использованы полтора раза
-augmentedString('q', 4, 'we'); // 'wweq' в этом примере выходит косяк)
-// Добавочные символы не использованы, исходная строка не изменена
-augmentedString('qwerty', 4, '0'); // 'qwerty'
+  total = originalLine;
+  const additionalCount = targetLength - originalLine.length;
+  const numberRepetitions = Math.floor(additionalCount / additionalStr.length);
+  const remainderDivision = additionalCount % additionalStr.length;
+
+  if ((additionalStr + originalLine).length > targetLength) {
+    total = additionalStr.slice(0, additionalCount) + total;
+
+  } else {
+    const mainСharacters = additionalStr.repeat(numberRepetitions);
+    const residualСharacters = additionalStr.slice(0, remainderDivision);
+    total = residualСharacters + mainСharacters + total;
+  }
+
+  return total;
+};
+
+console.log(returnsModifiedString('1', 2, '0')); // '01'
+console.log(returnsModifiedString('1', 4, '0')); // '0001' */
+console.log(returnsModifiedString('q', 4, 'werty')); // 'werq'
+console.log(returnsModifiedString('q', 4, 'we')); // 'wweq'
+console.log(returnsModifiedString('qwerty', 4, '0')); // 'qwerty'
 
 
 //Функция для проверки длины строки
 
-const lineLength = (stringChecked, maximumLength) => stringChecked.length <= maximumLength;
+const checkLineLength = (stringChecked, maximumLength) => stringChecked.length <= maximumLength; //сборка не дает сократить параметры до str и max
 
-lineLength('проверяемая строка', 20);
+/* console.log(checkLineLength('проверяемая строка', 20));
 
-lineLength('проверяемая строка', 18);
+console.log(checkLineLength('проверяемая строка', 18));
 
-lineLength('проверяемая строка', 10);
+console.log(checkLineLength('проверяемая строка', 10)); */
